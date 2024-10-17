d53bd8ea51df7ae14652a61306251222b8b7d194
kushkoshti22 <22it061@charusat.edu.in>
2024-10-17 22:10:08 +0530

    Add files via upload

---
+++ mern-app-main-main/mern-app-main-main/mern-app-main/server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(() => console.log('MongoDB connected'))
+   .catch(err => console.log(err));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+require('dotenv').config();
